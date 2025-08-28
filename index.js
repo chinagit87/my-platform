1ba4ba024fded1dd02a43703d9ebf97fc7e1f507
chinagit87 <emele.chinonyere.9@gmail.com>
2025-08-28 10:56:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("good morning");
+// alert(how are you);
+
+// var greet = "good afternoon";
+// var name = "Emele Chinonyerem";
+// var day = greet + ", how are you?";
+
+// alert(greet);
+// alert(day);
+// alert   (greet + name);
